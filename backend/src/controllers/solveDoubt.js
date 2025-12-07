ee728fe9b571bbd009252cfc677a89e4537b38a9
[shubhanshu rawat] <shubhanshurawat999@gmail.com>
2025-12-08 00:04:50 +0530

    changed the gemini model

--- backend/src/controllers/solveDoubt.js
+++ backend/src/controllers/solveDoubt.js
@@ -11,7 +11,7 @@ const solveDoubt = async(req , res)=>{
        
         async function main() {
         const response = await ai.models.generateContent({
-        model: "gemini-1.5-flash",
+        model: "gemini-2.5-flash",
         contents: messages,
         config: {
         systemInstruction: `
